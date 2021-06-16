759fa7fad78bb38d0f45bbc0747aedae02b155e9
Nathalie Krauth <krauthn@gmail.com>
2021-06-16 09:54:06 +0200

    dataset exploring

---
+++ R/exercises-wrangling.R
@@ -0,0 +1,16 @@
+source(here::here("R/package-loading.R"))
+
+# shows the column names of dataset
+colnames(NHANES)
+
+#structure
+str(NHANES)
+
+#takes a look at dataset
+glimpse(NHANES)
+
+#see summary
+summary(NHANES)
+
+# Look over the dataset documentation
+?NHANES
