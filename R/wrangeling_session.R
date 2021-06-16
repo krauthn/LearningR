a5450b686ea73f6baff87cf190eb9ba0e329aa1d
Nathalie Krauth <krauthn@gmail.com>
2021-06-16 10:11:24 +0200

    wrangling

--- R/wrangeling_session.R
+++ R/wrangeling_session.R
@@ -4,4 +4,21 @@ source(here::here("R/package-loading.R"))
 #briefly glimpse at content of dataset
 glimpse(NHANES)
 
+#select one column by its name
+select(NHANES, Age)
+
+#select more column by its name
+select(NHANES, Age, Weight, BMI)
+
+#exclude a column by its name
+select(NHANES, HeadCirc)
+
+#all coloumns starting with BP
+select(NHANES, starts_with("BP"))
+
+#all coloumns ending with Day
+select(NHANES, ends_with("Day"))
+
+#all coloumns containing Age
+select(NHANES, contains("AGE"))
 
